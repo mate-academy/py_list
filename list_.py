f0202942d87a8757f7e1ad4cf7c93b647c6195f1
Platoniuks <platon@alterpost.org>
2019-12-05 08:17:40 +0200

    resolved

--- list_.py
+++ list_.py
@@ -1,6 +1,47 @@
+"""Representation of singly linked list"""
+
+
 class Item:
-    pass
+    """Item of linked list"""
+    def __init__(self, val):
+        self._val = val
+        self._next = None
+
+    def get_value(self):
+        """Get value of the item"""
+        return self._val
+
+    def get_next(self):
+        """Get next item"""
+        return self._next
+
+    def set_next(self, val):
+        """Set link to the next item"""
+        self._next = val
 
 
 class List:
-    pass
+    """Class of linked list"""
+    def __init__(self):
+        self.head = None
+
+    def insert(self, val):
+        """Add item to the head of linked list"""
+        new_item = Item(val)
+        new_item.set_next(self.head)
+        self.head = new_item
+
+    def delete(self):
+        """Remove first element of linked list"""
+        del_item = self.head.get_value()
+        self.head = self.head.get_next()
+        return del_item
+
+    def state(self):
+        """Return state of linked list as string"""
+        current = self.head
+        state = []
+        while current:
+            state.append(current.get_value())
+            current = current.get_next()
+        return str(state)
